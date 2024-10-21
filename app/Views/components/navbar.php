3541af62beeef4033b2af1b9cfe4b24cf896f84e
YberaZakary <97361019+YberaZakary@users.noreply.github.com>
2024-10-22 00:41:23 +0800

    Fragment

---
+++ app/Views/components/navbar.php
@@ -0,0 +1,40 @@
+<body>
+
+<section>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary" >
+
+    <div class="container-fluid">
+      <a class="navbar-brand" href="<?= base_url('/'); ?>"><img src="<?= base_url('assets/images/logo.png')?>" class="logo" style="width:100px; margin-left: 50px;" ></a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="<?= base_url('/'); ?>" style="font-family: raleway;" >Home</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('/catalog'); ?>" style="font-family: raleway;">Catalog</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('/about'); ?>" style="font-family: raleway;" >About</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('/contact'); ?>" style="font-family: raleway;" >Contact</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="d-flex">
+        <a href="<?= base_url('/cart'); ?>" class="cart-icon">
+          <i class="bi bi-cart4" style="font-size: 30px; color: rgb(210,235,255);"></i>
+        </a>
+    </div>
+
+  </nav>
+</section>
--- app/Views/components/top_layer.php
+++ app/Views/components/top_layer.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="UTF-8">
+<header>
+    <meta charset="UTF-8">
     <title>Home</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -27,43 +27,4 @@
     <link rel="stylesheet" href="<?= base_url('assets/css/maincss.css') ?>">
     <script type="text/javascript"></script>
 
-</head>
-<body>
-    
-<nav class="navbar navbar-expand-lg bg-body-tertiary" >
-
-<div class="container-fluid">
-  <a class="navbar-brand" href="<?= base_url('/'); ?>"><img src="<?= base_url('assets/images/logo.png')?>" class="logo" style="width:100px; margin-left: 50px;" ></a>
-
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" >
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-
-      <li class="nav-item">
-        <a class="nav-link" aria-current="page" href="<?= base_url('/'); ?>" style="font-family: raleway;" >Home</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('/catalog'); ?>" style="font-family: raleway;">Catalog</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('/about'); ?>" style="font-family: raleway;" >About</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('/contact'); ?>" style="font-family: raleway;" >Contact</a>
-      </li>
-
-    </ul>
-  </div>
-
-</div>
-
-</nav>
-
-</body>
-</html>
+</header>
