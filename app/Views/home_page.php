969c35705e0d4c2f9cde5eb9670cb06cf7b04bc2
Rei Batoon <rei.batoon24@gmail.com>
2024-10-21 23:29:20 +0800

    updated pic

--- app/Views/home_page.php
+++ app/Views/home_page.php
@@ -70,7 +70,7 @@
   <section>
     <div class="uk-background-cover d-flex flex-column justify-content-center align-items-center" data-src="<?= base_url('assets/images/neighborhood.png')?>" style="height: 700px; object-fit: fill;" uk-img>
       <h1 style="font-family: DynaPuff; font-size: 100px; color:rgb(43, 44, 65); text-shadow: 2px 2px rgba(0, 0, 0, 0.432);">The Bluey Shop</h1>
-      <button class="zoom viewmenu mt-3">
+      <button class="viewmenu mt-3">
         <a href="<?= base_url('/catalog'); ?>" style="color: rgb(47, 62, 70);">Go To Catalog</a>
       </button>
     </div>
@@ -90,7 +90,7 @@
   </section>
 
   <section>
-  <div class="uk-background-cover d-flex flex-column justify-content-center align-items-center" data-src="<?= base_url('assets/images/trio2.png')?>" style="max-width: 50%; margin: auto; height: 600px; object-fit: fill;" uk-img></div>
+  <div class="uk-background-cover d-flex flex-column justify-content-center align-items-center" data-src="<?= base_url('assets/images/trio1.png')?>" style="max-width: 50%; margin: auto; height: 600px; object-fit: fill;" uk-img></div>
   </div>
   </section>
 
