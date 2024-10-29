2e19810e80fbcc28d418b9db69da6ea3568b6eed
Rei Batoon <rei.batoon24@gmail.com>
2024-10-30 03:02:09 +0800

    Update contact_page.php

--- app/Views/contact_page.php
+++ app/Views/contact_page.php
@@ -13,8 +13,9 @@
       <h1 style="font-family: DynaPuff; font-size: 100px; font-weight: bold; color:rgb(43, 44, 65); text-shadow: 2px 2px rgba(0, 0, 0, 0.432);">Contact Us</h1>
     </div>
 
-    <article class="uk-comment" role="comment" style = "padding: 20px; font-family: DynaPuff;">
-    <header class="uk-comment-header">
+    <div style="max-width: 60%; margin: auto;">
+      <article class="uk-comment" role="comment" style = "padding: 20px; font-family: DynaPuff;">
+      <header class="uk-comment-header">
         <div class="uk-grid-medium uk-flex-middle" uk-grid>
             <div class="uk-width-auto">
               <img class="card-img" src="<?= base_url('assets/images/FamilyPicAbout.png')?>" alt="Card image cap" style="height: 350px; width: 300px; object-fit: cover;">
@@ -29,24 +30,24 @@
                     </div>
                 </div>
             </div>
-    </header>
-  </article>
-
-  <article class="uk-comment" role="comment" style = "padding: 20px; font-family: DynaPuff;">
-  <header class="uk-comment-header">
-        <div class="uk-grid-medium uk-flex-middle" uk-grid>
-            <div class="uk-width-auto">
-              <img class="card-img" src="<?= base_url('assets/images/ReadingBook.png')?>" alt="Card image cap" style="height: 350px; width: 300px; object-fit: cover;" >
-                </div>
-                <div class="uk-width-expand">
-                    <h4 class="uk-comment-title uk-margin-remove" style = "font-family: DynaPuff; font-size: 34px;">Our Contacts</h4>
-                    <div class="uk-comment-body">
-                      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </header>
+      </article>
+      <article class="uk-comment" role="comment" style = "padding: 20px; font-family: DynaPuff;">
+      <header class="uk-comment-header">
+            <div class="uk-grid-medium uk-flex-middle" uk-grid>
+                <div class="uk-width-auto">
+                  <img class="card-img" src="<?= base_url('assets/images/ReadingBook.png')?>" alt="Card image cap" style="height: 350px; width: 300px; object-fit: cover;" >
+                    </div>
+                    <div class="uk-width-expand">
+                        <h4 class="uk-comment-title uk-margin-remove" style = "font-family: DynaPuff; font-size: 34px;">Our Contacts</h4>
+                        <div class="uk-comment-body">
+                          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-    </header>
-  </article>
+        </header>
+      </article>
+    </div>
 
 
   </section>
